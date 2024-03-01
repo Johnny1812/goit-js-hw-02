d028753984b35cc811268a257fcbbf40c3c04fd9
Johnny1812 <153526415+Johnny1812@users.noreply.github.com>
2024-03-01 15:15:03 +0200

    t1

---
+++ js/task-4.js
@@ -0,0 +1,7 @@
+
+console.log(getShippingCost("Australia"));
+console.log(getShippingCost("Germany"));
+console.log(getShippingCost("China")); 
+console.log(getShippingCost("Chile")); 
+console.log(getShippingCost("Jamaica")); 
+console.log(getShippingCost("Sweden")); 
