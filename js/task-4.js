f9119b43ab3448f15fcb37290a93e261c72f0464
Radomskyi Ivan <good.day.by.radomskyi@gmail.com>
2024-08-27 09:52:35 +0300

    t1

--- js/task-4.js
+++ js/task-4.js
@@ -1,7 +1,1 @@
 
-console.log(getShippingCost("Australia"));
-console.log(getShippingCost("Germany"));
-console.log(getShippingCost("China")); 
-console.log(getShippingCost("Chile")); 
-console.log(getShippingCost("Jamaica")); 
-console.log(getShippingCost("Sweden")); 
